80cc3e3ba3c5d90e2dfda94b1759f168e5ff85ae
Keyaan <88016629+Keyaan-07@users.noreply.github.com>
2024-09-01 00:29:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def userMenu():
     print("--------------------------")
 
     if userChoice == 1:
-        addNote() #upcoming function
+        addNote() # upcoming function
     elif userChoice == 2:
         home() # upcoming function
     elif userChoice == 3:
@@ -117,7 +117,7 @@ def searchBooksMenu():
     elif userChoice == 2:
         home() # upcoming function
     elif userChoice == 3:
-        searchBooks() # upcoming function
+        searchBooks()
     elif userChoice == 4:
         exiting()
     else:
@@ -184,3 +184,151 @@ def searchBooksbyKeyword():
         print(f'No books found with the keyword "{keyword}".')
         print("--------------------------")
         searchBooksMenu()
+
+# Function to display search options for books
+def searchBooks():
+    print("--------------------------")
+    print("Search Books")
+    print("--------------------------")
+    print("1. Search by Book ID")
+    print("2. Search by Keyword")
+    print("3. Home")
+    print("4. Back")
+    print("5. Exit")
+    userChoice = int(input("Enter your Choice to Continue : "))
+    print("--------------------------")
+
+    if userChoice == 1:
+        searchBooksbyId()
+    elif userChoice == 2:
+        searchBooksbyKeyword()
+    elif userChoice == 3:
+        home() # upcoming function
+    elif userChoice == 4:
+        user() # upcoming function
+    elif userChoice == 5:
+        exiting()
+    else:
+        validOption()
+
+# Function to display the add book menu and handle user choices
+def addBookMenu():
+	# Add book menu options
+	print("1. Home")
+	print("2. Back")
+	print("3. Exit")
+	userChoice = int(input("Enter your Choice to Continue : "))
+	print("--------------------------")
+    # User choices handling
+	if userChoice == 1:
+		home() # upcoming function
+	elif userChoice == 2:
+		modifyBook() # upcoming function
+	elif userChoice == 3:
+		exiting()
+	else:
+		validOption()
+
+# Function to add a new book to the library
+def addBook():
+    print("--------------------------")
+    print("Add Book")
+    print("--------------------------")
+    bookId = int(input("Enter the Book ID : "))
+    bookName = input("Enter the Book Name : ")
+    publicationYear = int(input("Enter the Book Publication Year : "))
+    author = input("Enter the Book Author Name : ")
+    print("--------------------------")
+
+    c.execute("SELECT bookId FROM books")
+    result = c.fetchall()
+    db.commit()
+
+    if (bookId,) in result:
+        print(f'The book of book id "{bookId}" is already available in the digital library.')
+        print("--------------------------")
+        addBookMenu()
+    else:
+        c.execute(
+            "INSERT INTO books (bookId, bookName, publicationYear, author) VALUES (%s, %s, %s, %s)",
+            (bookId, bookName, publicationYear, author),
+        )
+        db.commit()
+
+        print("Book added Successfully!")
+        print("--------------------------")
+        addBookMenu()
+
+def deleteBookMenu():
+	# Delete book menu options
+	print("1. Home")
+	print("2. Back")
+	print("3. Exit")
+	userChoice = int(input("Enter your Choice to Continue : "))
+	print("--------------------------")
+
+	# User choices handling
+	if userChoice == 1:
+		home() # upcoming function
+	elif userChoice == 2:
+		admin() # upcoming function
+	elif userChoice == 3:
+		exiting()
+	else:
+		validOption()
+
+# Function to delete a book from the library
+def deleteBook():
+    print("--------------------------")
+    print("Delete Book")
+    print("--------------------------")
+    bookId = int(input("Enter the Book ID : "))
+    choice = input("Are you sure to delete the Book? (Yes/No) : ")
+    print("--------------------------")
+
+    c.execute("SELECT bookId FROM books")
+    result = c.fetchall()
+    db.commit()
+
+    if choice.lower() in ["yes", "y"]:
+        if (bookId,) in result:
+            c.execute("DELETE FROM books WHERE bookId=%s", (bookId,))
+            db.commit()
+
+            print("Book deleted Successfully!")
+            print("--------------------------")
+            deleteBookMenu()
+        else:
+            print(f'The book of book id "{bookId}" does not exist in the digital library.')
+            print("--------------------------")
+            deleteBookMenu()
+    elif choice.lower() in ["no", "n"]:
+        print("--------------------------")
+        print("Book Not Deleted!")
+        print("--------------------------")
+        deleteBookMenu()
+    else:
+        validOption()
+
+# Update book menu options
+def updateBookMenu():
+	print("1. Home")
+	print("2. Back")
+	print("3. Exit")
+	userChoice = int(input("Enter your Choice to Continue : "))
+	print("--------------------------")
+
+	# User choices handling
+	if userChoice == 1:
+		home() # upcoming function
+	elif userChoice == 2:
+		updateUser() # upcoming function
+	elif userChoice == 3:
+		exiting()
+	else:
+		validOption()
+
+def notBook(bookId):
+	print(f'The book of book id "{bookId}" does not available in the digital library.')
+	print("--------------------------")
+	updateBookMenu()
